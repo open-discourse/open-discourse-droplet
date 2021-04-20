967bfd24b8b64c86cb92e2c339ea49f1aeb113ec
Jakob Kraus <jakob.kraus10@gmail.com>
2021-04-20 11:13:04 +0200

    Updated persistent_database

--- persistent_database.sql
+++ persistent_database.sql
@@ -7,3 +7,11 @@ CREATE TABLE misc.fts_tracking
 	"timestamp" TIMESTAMP DEFAULT now(),
 	CONSTRAINT fts_tracking_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE misc.topic_tracking
+(
+	id bigserial NOT NULL,
+	search_query text NOT NULL,
+	"timestamp" TIMESTAMP DEFAULT now(),
+	CONSTRAINT topic_tracking_pk PRIMARY KEY (id)
+);
